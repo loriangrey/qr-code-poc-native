2072a92dbea01272a556ade9c0f5e6cd6ffe25c3
loriangrey <147693327+loriangrey@users.noreply.github.com>
2024-01-03 13:21:39 +0100

    Delete Dyson/Dysonapp directory

--- Dyson/Dysonapp/src/routes/+page.svelte
+++
@@ -1,8 +0,0 @@
-<script>
-    import "./styles.scss"
-</script>
-
-<div class="page-wrapper">
-    <h1>Welcome to Dyson Products Review</h1>
-    <p>Visit the <a href="/products"> Products Page</a></p>
-</div>
--- Dyson/Dysonapp/src/routes/products/+page.svelte
+++
@@ -1,9 +0,0 @@
-<script>
-    import Products from "../../components/Products.svelte"
-    import "../styles.scss"
-</script>
-
-<div class="page-wrapper">
-    <h1>Products</h1>
-    <Products />
-</div>
